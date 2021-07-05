e4f8e9f4e375992b84435cd6425552db4f32072e
duyettwyler2k <duyettwyller@gmail.com>
2021-07-05 17:47:52 +0700

    firstcommit

---
+++ src/main/java/com/codegym/project/controller/admin/UserRestController.java
@@ -0,0 +1,65 @@
+package com.codegym.project.controller.admin;
+
+import com.codegym.project.model.Address;
+import com.codegym.project.model.User;
+import com.codegym.project.service.address.IAddressService;
+import com.codegym.project.service.user.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/admin/user")
+public class UserRestController {
+    @Autowired
+    private IUserService userService;
+    @GetMapping
+    public ResponseEntity<Iterable<User>> showListUser(){
+        List<User>users= (List<User>) userService.findAll();
+        if (users.isEmpty()){
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+    @GetMapping("/listUser")
+    public ModelAndView showAllUser(){
+        ModelAndView modelAndView=new ModelAndView("/admin/user/listUser");
+        modelAndView.addObject("users",userService.findAll());
+        return modelAndView;
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findByIdUser(@PathVariable Long id){
+        Optional<User>userOptional=userService.findById(id);
+        if (!userOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(userOptional.get(),HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<User>createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.save(user),HttpStatus.CREATED);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id){
+        Optional<User>userOptional=userService.findById(id);
+        if (!userOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        userService.remove(id);
+        return new ResponseEntity<>(userOptional.get(),HttpStatus.NO_CONTENT);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<User> editUser(@PathVariable Long id,@RequestBody User user){
+        Optional<User>userOptional=userService.findById(id);
+        if (!userOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        user.setId(userOptional.get().getId());
+        return new ResponseEntity<>(userService.save(user),HttpStatus.OK);
+    }
+}
